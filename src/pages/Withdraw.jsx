76ea0b1dcd4b8e1cfddb63546c27ef0b88498978
katembasolomon7-byte <katembasolomon7@gmail.com>
2026-06-07 16:26:00 +0300

    Update Withdraw.jsx

--- src/pages/Withdraw.jsx
+++ src/pages/Withdraw.jsx
@@ -53,7 +53,7 @@ export default function Withdraw() {
       return;
     }
     const token = localStorage.getItem("authToken");
-    const BASE_URL = "https://stacksapp-backend.onrender.com";
+    const BASE_URL = "https://earthbound-admin.onrender.com";
     const res = await fetch(`${BASE_URL}/api/withdraw`, {
       method: "POST",
       headers: {
